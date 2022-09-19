b2c06e58ce7ea31709be2bf195cd236228afe874
Salih Kara <ilimyiyen@gmail.com>
2022-09-19 18:07:47 +0300

    Hesap Makinesi

--- ana_dosya.py
+++ ana_dosya.py
@@ -1,1 +1,15 @@
 print("hello world")
+print("""
+HESAP MAKİNESİ
+TOPLAMA İŞLEMİ YAPMAK İÇİN 1 'e BASIN.
+ÇIKARMA İŞLEMİ YAPMAK İÇİN 2 'e BASIN.
+""")
+islem = str(input("İşlem seçiniz: "))
+if islem == "1":
+    sayi1 = int(input("sayi1 giriniz: "))
+    sayi2 = int(input("sayi2 giriniz: "))
+    print("Sonuç:", sayi1 + sayi2)
+if  islem == "2":
+    sayi1 = int(input("sayi1 giriniz: "))
+    sayi2 = int(input("sayi2 giriniz: "))
+    print("Sonuç:", sayi1 - sayi2)
